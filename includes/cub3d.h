547bb1cadbffbef5730dbbf7d276ba372e8505e1
Joao Pedro Freire Vieira <jpedro-f@c2r3s10.42porto.com>
2025-10-17 19:47:06 +0100

    draw player

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,14 +6,37 @@
 /*   By: jpedro-f <jpedro-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 17:13:31 by jpedro-f          #+#    #+#             */
-/*   Updated: 2025/10/03 17:14:11 by jpedro-f         ###   ########.fr       */
+/*   Updated: 2025/10/17 19:39:33 by jpedro-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 #define CUB3D_H
 
+#include "../minilibx/mlx.h"
 
+typedef struct s_player
+{
+	double	x;
+	double	y;
+}	t_player;
+
+
+typedef struct s_game
+{
+	void		*mlx;
+	void		*mlx_win;
+}	t_game;
+
+typedef struct s_img
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+	t_game	*game;
+}	t_img;
 
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,18 +6,66 @@
 /*   By: jpedro-f <jpedro-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 17:11:31 by jpedro-f          #+#    #+#             */
-/*   Updated: 2025/10/03 18:36:22 by jpedro-f         ###   ########.fr       */
+/*   Updated: 2025/10/17 19:44:17 by jpedro-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
+void img_init(t_game *game, t_img *img)
+{
+	img->img = mlx_new_image(game->mlx, 1280, 720);
+	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
+	img->game = game;
+}
+
+void pixel_put(t_img *img, int x, int y, int color)
+{
+	char *dst;
+	
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void player_init(t_player *player) 
+{
+	player->x = 300;
+	player->y = 300;
+}
+
+void	draw_player(t_img *img, t_player *player, int color)
+{
+	int size = 50;
+	int y; 
+	int x;
+
+	y = (int)player->y - size / 2;
+	while (y < (int)player->y + size / 2)
+	{
+		x = (int)player->x - size / 2;
+		while (x < (int)player->x + size / 2)
+		{
+			pixel_put(img, x, y, color);
+			x++;
+		}
+		y++;
+	}
+	
+}
+
 int main (void)
 {
+	t_game	game;
+	t_img	img;
+	t_player player;
 
-
-
-
+	player_init(&player);
+	game.mlx = mlx_init();
+	game.mlx_win = mlx_new_window(game.mlx, 1280, 720, "cub3d");
+	img_init(&game, &img);
+	draw_player(&img, &player, 0x00FF0000);
+	mlx_put_image_to_window(game.mlx, game.mlx_win, img.img, 0, 0);
+	mlx_loop(game.mlx);
 
 	return (0);
 }
