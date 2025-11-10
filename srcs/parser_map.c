edd8ef4768ea03756b29527a0a1f13c398ae6d5a
Gabriela Goncalves De Oliveira <ggoncalv@c2r1s2.42porto.com>
2025-11-10 14:58:18 +0000

    feat: created aux ft calculate_max_width to improve code clarity

--- srcs/parser_map.c
+++ srcs/parser_map.c
@@ -26,21 +26,31 @@ void    read_map(t_map *map_info, int *width, int *height)
 {
     char    *map;
 
-    *width = ft_strlen(map_info->line);
     *height = 1;
+	*width	= 0;
     map_info->tmp = get_next_line(map_info->filename_fd);
     while(map_info->tmp)
     {
+		calculate_max_width(width, map_info->tmp);
         check_map_line_validity(map_info->tmp, map_info);
         (*height)++;
-        if ((int)ft_strlen(map_info->tmp) > *width)
-            *width = ft_strlen(map_info->tmp);
         map = ft_strjoin(map_info->line, map_info->tmp);
         free(map_info->line);
         free(map_info->tmp);
         map_info->line = map;
         map_info->tmp = get_next_line(map_info->filename_fd);
     }
+}
+
+void	calculate_max_width(int	*width, char *curr_line)
+{
+    int		line_len;
+
+	line_len = ft_strlen(curr_line);
+	if (line_len > 0 && curr_line[line_len - 1] == '\n')
+		line_len--;
+    if (line_len > *width)
+            *width = line_len;
 }
 
 void    check_map_line_validity(char *line, t_map *map_info)
@@ -66,46 +76,3 @@ void    check_map_line_validity(char *line, t_map *map_info)
     if (line [i - 1] != '\n' && line[i] == '\0' && player_count == 0)
         free_exit("No player start position found in map", map_info, 1);
 }
-
-/*char    **split_map_lines(char *map_str, int *width, int *height)
-{
-    char    **matrix;
-    int     i;
-
-    matrix = (char **)malloc(sizeof(char *) * (height + 1));
-    if (!matrix)
-        return (free_exit("Memory allocation failed", map_info, 1), NULL);
-    i = 0;
-    while (i < *height)
-    {
-        matrix[i] = get_matrix_line(&map_str, *width);
-        
-    }
-
-}
-
-char    *get_matrix_line(char **map_str, int width)
-{
-    char    *line;
-    int     i;
-
-    line = malloc(sizeof(char) * (width + 1));
-    if (!line)
-        return (NULL);
-    i = 0;
-    while (*map_str && **map_str != '\n' || **map_str != '\r')
-        i++;
-    ft_strlcpy(line, *map_str, i);
-    if ( *map_str)
-        *map_str += i + 1; // Skip newline character
-    if (i < width)
-    {
-        while (i < width)
-        {
-            line[i] = ' ';
-            i++;
-        }
-    }
-    line[i] = '\0';
-
-}*/
